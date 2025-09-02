e9476808af4d25a31dcf99765a103882fd3c1ae3
pfackeldey <fackeldey.peter@gmail.com>
2025-09-02 14:39:19 +0200

    setup project

---
+++ src/everwillow/__init__.py
@@ -0,0 +1,14 @@
+"""
+everwillow: statistical inference steps for high-energy physics analyses with JAX.
+"""
+
+from __future__ import annotations
+
+import datetime
+
+__name__ = "everwillow"
+__author__ = "everwillow developers"
+__copyright__ = f"Copyright {datetime.datetime.now().year}, everwillow developers"
+__credits__ = ["Mohamed Aly", "Peter Fackeldey", "Massimiliano Galli"]
+__contact__ = "https://github.com/MoAly98/everwillow"
+__version__ = "0.0.1"

0993c2dc4bd7664dfbeceecd4e5da1dc551a1a5d
pfackeldey <fackeldey.peter@gmail.com>
2025-11-01 21:28:47 +0100

    fix doc string

--- src/everwillow/parameters/bounds.py
+++ src/everwillow/parameters/bounds.py
@@ -2,9 +2,9 @@
 Example
 -------
 >>> state = sl.State.from_pytree({"mu": 0.3})
->>> transform = transforms.MinuitTransform(lower=0.0, upper=1.0)
->>> unwrapped = unwrap(state, {("mu",): transform})
->>> jnp.isclose(wrap(unwrapped, {("mu",): transform)[("mu",)], state[("mu",)])
+>>> transform_map = {("mu",): transforms.MinuitTransform(lower=0.0, upper=1.0)}
+>>> unwrapped = unwrap(state, transform_map)
+>>> jnp.isclose(wrap(unwrapped, transform_map)[("mu",)], state[("mu",)])
 Array(True, dtype=bool)
 """
 

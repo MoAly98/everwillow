da0e861cb2a940ec6e680a249b3f597c5da783d1
Mohamed Aly <mohamedaymanali@gmail.com>
2026-07-10 15:51:13 +0200

    refactor!: remove the objective-based upper limit free functions

--- src/everwillow/hypotest/__init__.py
+++ src/everwillow/hypotest/__init__.py
@@ -5,5 +5,5 @@ Import from submodules directly::
     from everwillow.hypotest.test_statistics import QTilde
     from everwillow.hypotest.calculators import AsymptoticCalculator
     from everwillow.hypotest.distributions import QTildeAsymptotic
-    from everwillow.hypotest.upper_limit import upper_limit
+    from everwillow.hypotest.limit_solvers import RootFindingLimitSolver
 """
